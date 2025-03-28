1b73408e5ee41b660337456e865bc14ad29a5ac9
huo65 <1599272181@qq.com>
2025-03-28 20:53:28 +0800

    fix: 修复重复配置

--- master_data/src/main/java/cn/xidian/master_data/MasterDataApplication.java
+++ master_data/src/main/java/cn/xidian/master_data/MasterDataApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("cn.xidian.master_data.mapper")
 public class MasterDataApplication {
 
     public static void main(String[] args) {
